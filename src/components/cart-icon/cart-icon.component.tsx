63c9567b79d9b94971aefa12f8a20a5ec60b2d77
Carlos Dominguez Puig <carlos.dom13@hotmail.com>
2020-06-18 16:07:26 +0200

    numero en el carrito

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -4,13 +4,21 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { ReactComponent as ShoppingIcon } from '../../assets/shoppping-bag.svg';
 import './cart-icon.styles.scss';
 import { CartActionTypes } from '../../redux/cart/cart.types';
-const CartIcon = ({ toggleCartHidden }: any) => (
+interface CartIconProps {
+    toggleCartHidden: any,
+    itemCount: number
+}
+const CartIcon = ({ toggleCartHidden, itemCount }: CartIconProps) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon"></ShoppingIcon>
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 );
+
 const mapDispatchToProps = (dispatch: (arg0: { type: CartActionTypes }) => any) => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
-export default connect(null, mapDispatchToProps)(CartIcon)
+const mapStateToProps = ({ cart: { cartItems } }: any) => ({
+    itemCount: cartItems.reduce((accumulatedQuantity: any, cartItems: any) => accumulatedQuantity + cartItems.quantity, 0)
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
---
+++ src/redux/cart/cart.selector.ts
@@ -0,0 +1,18 @@
+import { createSelector } from 'reselect';
+
+const selectCart = (state: { cart: any; }) => state.cart;
+
+export const selectCartItems = createSelector(
+    [selectCart],
+    cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce(
+            (accumalatedQuantity: number, cartItem: { quantity: number; }) =>
+                accumalatedQuantity + cartItem.quantity,
+            0
+        )
+);
